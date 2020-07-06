1d230cd28d799300665c7d1db5f8035c6b745c07
Sercan Altun <altun.sercan@gmail.com>
2020-07-05 22:33:27 -0400

    Add KegBase implementation

---
+++ Core/Runtime/Keg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kegstand
+{
+    public interface Keg
+    {
+        float MaxAmount { get; }
+        float MinAmount { get; }
+        float Amount { get; }
+        void Increment(float delta);
+        void Decrement(float decrement);
+    }
+    
+    public class KegBase : Keg
+    {
+        public float MaxAmount { get; private set; }
+        public float MinAmount { get; private set; }
+        public float Amount { get; private set; }
+
+        public KegBase(float maxAmount, float minAmount, float startingAmount)
+        {
+            MaxAmount = maxAmount;
+            MinAmount = minAmount;
+            Amount = startingAmount;
+        }
+
+
+        public void Increment(float delta)
+        {
+            if (delta < 0f)
+            {
+                throw new ArgumentException("Argument should not be less than zero", nameof(delta));
+            }
+            
+            Amount = Mathf.Min(Amount + delta, MaxAmount);
+        }
+
+        public void Decrement(float delta)
+        {
+            if (delta < 0f)
+            {
+                throw new ArgumentException("Argument should not be less than zero", nameof(delta));
+            }
+            
+            Amount = Mathf.Max(Amount - delta, MinAmount);
+        }
+    }
+}
---
+++ Core/Tests/KegUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Kegstand;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class KegUnitTests
+    {
+        [Test]
+        [TestCase(100f, 10f, 10f, 20f)]
+        [TestCase(100f,20f, 0f, 20f)]
+        [TestCase(30f,20f, 20f, 30f)]
+        public void CanIncrement(float maxAmount, float startingAmount, float increment, float result )
+        {
+            // Given
+            KegBase keg = new KegBase( maxAmount,0f, startingAmount );
+
+            // When
+            keg.Increment(increment);
+
+            // Then
+            Assert.AreEqual(keg.Amount, result);
+        }
+        
+        [Test]
+        public void CannotIncrementWithNegative()
+        {
+            // Given
+            KegBase keg = new KegBase( 100f,0f, 50f );
+
+            // When & Then
+            Assert.Throws<ArgumentException>(() => keg.Increment(-10f));
+        }
+        
+        
+        [Test]
+        [TestCase(0f, 10f, 5f, 5f)]
+        [TestCase(20f, 30f, 20f, 20f)]
+        public void CanDecrementFluid(float minAmount, float startingAmount, float decrement, float result )
+        {
+            // Given
+            KegBase keg = new KegBase( 100f, minAmount, startingAmount );
+
+            // When
+            keg.Decrement(decrement);
+
+            // Then
+            Assert.AreEqual(keg.Amount, result);
+        }
+        
+        [Test]
+        public void CannotDecrementWithNegative()
+        {
+            // Given
+            KegBase keg = new KegBase( 100f,0f, 50f );
+
+            // When & Then
+            Assert.Throws<ArgumentException>(() => keg.Decrement(-10f));
+        }
+
+
+    }
+}
