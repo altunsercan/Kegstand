36c2c00ac53a258bc3680f4c0017f9b3f6d095a0
Sercan Altun <altun.sercan@gmail.com>
2020-07-24 00:45:43 -0400

    Add ordered events

---
+++ Core/Runtime/Simulator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kegstand
+{
+    public class TimedEvent
+    {
+        public float Time { get; set; }
+        public int Index { get; set; }
+    }
+    
+    public class Simulator
+    {
+        
+        public IEnumerable<TimedEvent> Events { get; private set; }
+        private readonly List<TimedEvent> events = new List<TimedEvent>();
+        public Simulator()
+        {
+            Events = events;
+        }
+        
+        public void AddEvent(float time, int index)
+        {
+            events.Add(new TimedEvent(){Time = time, Index = index});
+            events.Sort((x,y)=>(x.Time == y.Time)?0:(x.Time>y.Time)?1:-1);
+        }
+    }
+}
---
+++ Core/Tests/SimulatorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Kegstand.Tests
+{
+    public class SimulatorTests
+    {
+        [Test]
+        [TestCase(20,3,4,12,5,67)]
+        [TestCase(20,-5,4,-2,5,23)]
+        public void ShouldMaintainTimeOrderedEvents(params float[] eventTimes)
+        {
+            // Given
+            Simulator simulator = new Simulator();
+            
+            // When
+            for (var index = 0; index < eventTimes.Length; index++)
+            {
+                float t = eventTimes[index];
+                simulator.AddEvent(t, index);
+            }            
+            
+            // Then
+            Assert.That(simulator.Events, Is.Ordered.By("Time"));                
+        }
+    }
+}
