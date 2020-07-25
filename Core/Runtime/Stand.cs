7a933f25961fa7ba1eb6c69330fad8a4dc3e1e9f
Sercan Altun <altun.sercan@gmail.com>
2020-07-25 00:19:26 -0400

    Add setting up stands

--- Core/Runtime/Stand.cs
+++ Core/Runtime/Stand.cs
@@ -1,7 +1,25 @@
-﻿namespace Kegstand
+﻿using System.Collections.Generic;
+
+namespace Kegstand
 {
     public class Stand
     {
+        private readonly Dictionary<object, Keg> kegs = new Dictionary<object, Keg>();
         
+        public void AddKeg(object uniqueObj, Keg keg)
+        {
+            if (kegs.ContainsKey(uniqueObj))
+            {
+                return;
+            }
+            kegs.Add(uniqueObj, keg);
+        }
+
+        public object GetKeg(object uniqueObj)
+        {
+            Keg keg = null;
+            kegs.TryGetValue(uniqueObj, out keg);
+            return keg;
+        }
     }
 }
---
+++ Core/Tests/StandTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace Kegstand.Tests
+{
+    public class StandTests
+    {
+        private KegBase.Builder<KegBase> kegBuilder;
+        
+        [SetUp]
+        public void Setup()
+        {
+            var calculator = new FlowCalculatorImpl();
+            kegBuilder = new KegBase.Builder<KegBase>(flowCalculator:calculator);
+        }
+        
+        [Test]
+        public void ShouldRegisterKegsWithUniqueId()
+        {
+            // Given
+            Stand stand = new Stand();
+            Keg keg = kegBuilder.Build();
+            Keg keg2 = kegBuilder.Build();
+            object uniqueObj = new object();
+            object uniqueObj2 = new object();
+            // When
+            stand.AddKeg(uniqueObj, keg);
+            stand.AddKeg(uniqueObj2, keg2);
+
+            // Then
+            Assert.AreEqual(keg, stand.GetKeg(uniqueObj));
+            Assert.AreEqual(keg2, stand.GetKeg(uniqueObj2));
+        }
+        
+        [Test]
+        public void ShouldNotRegisterKegsWithoutUniqueId()
+        {
+            // Given
+            Stand stand = new Stand();
+            Keg keg = kegBuilder.Build();
+            Keg keg2 = kegBuilder.Build();
+            object uniqueObj = new object();
+            // When
+            stand.AddKeg(uniqueObj, keg);
+            stand.AddKeg(uniqueObj, keg2);
+
+            // Then
+            Assert.AreEqual(keg, stand.GetKeg(uniqueObj));
+            Assert.AreNotEqual(keg2, stand.GetKeg(uniqueObj));
+        }
+        
+    }
+}
