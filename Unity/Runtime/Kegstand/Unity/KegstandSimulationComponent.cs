06a74f5a3a422e792d0de3b2815a4790bc47f26f
Sercan Altun <altun.sercan@gmail.com>
2020-08-03 23:12:47 -0400

    Add simulation component

---
+++ Unity/Runtime/Kegstand/Unity/KegstandSimulationComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Kegstand.Unity
+{
+    public class KegstandSimulationComponent : MonoBehaviour
+    {
+        public bool AutoRegisterComponentsInScene = true;
+        public Simulator Simulator { get; private set; }
+        public IReadOnlyList<Stand> Stands => Simulator.Stands;
+
+        public void Awake()
+        {
+            Simulator = new Simulator();
+        }
+
+        private void Start()
+        {
+            if (AutoRegisterComponentsInScene)
+            {
+                FindExistingKegstandComponentsInScene();
+            }
+            
+        }
+
+        private void FindExistingKegstandComponentsInScene()
+        {
+            var rootObjects = gameObject.scene.GetRootGameObjects();
+            foreach (GameObject rootGObj in rootObjects)
+            {
+                AddStandFromGameObject(rootGObj);
+            }
+        }
+
+        private void AddStandFromGameObject(GameObject gObj)
+        {
+            if (gObj.GetComponent<Stand>() is Stand stand)
+            {
+                if (stand is IWrapperComponent<Stand> standWrapper)
+                {
+                    var standBuilder = new StandBase.Builder();
+                    var pureStand = standBuilder.Build();
+                    standWrapper.SetWrappedObject(pureStand);      
+                }
+                
+                Simulator.Register(stand);
+            }
+        }
+    }
+}
---
+++ Unity/Tests/Kegstand/Unity/KegstandSimulatorComponentTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Kegstand.Unity
+{
+    public class KegstandSimulatorComponentTests
+    {
+        private KegstandSimulationComponent simulationComp;
+
+        private void MakeSimulator(bool inactive = false)
+        {
+            GameObject simulatorGobj = new GameObject("Simulator");
+            if (inactive)
+            {
+                simulatorGobj.SetActive(false);
+            }
+            simulationComp = simulatorGobj.AddComponent<KegstandSimulationComponent>();
+        }
+        
+        private StandComponent MakeStandObject(string name = "Stand")
+        {
+            GameObject standGameObj = new GameObject(name);
+            return standGameObj.AddComponent<StandComponent>();
+        }
+
+        private KegComponent MakeKegObject(string name = "Keg")
+        {
+            GameObject kegGameObj = new GameObject(name);
+            return kegGameObj.AddComponent<KegComponent>();
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            yield return new ExitPlayMode();
+            
+            simulationComp = null;
+            
+            var sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+            for (var index = sceneObjects.Length - 1; index >= 1; index--) // Don't destroy first object which is test runner
+            {
+                GameObject obj = sceneObjects[index];
+
+                GameObject.Destroy(obj);
+            }
+
+            yield return null;
+        }
+        
+
+        [UnityTest]
+        public IEnumerator ShouldCreateSimulationOnStart()
+        {
+            yield return null;
+
+            MakeSimulator(inactive: true);
+            
+            Assert.IsNull(simulationComp.Simulator);
+            simulationComp.Awake();
+            Assert.IsNotNull(simulationComp.Simulator);
+        }
+
+
+        [UnityTest]
+        public IEnumerator ShouldAutoDiscoverExistingComponentsInScene()
+        {
+            yield return null;
+            
+            MakeSimulator();
+            simulationComp.AutoRegisterComponentsInScene = true;
+
+            StandComponent stand = MakeStandObject();
+            
+            yield return new EnterPlayMode();
+            
+            Assert.AreEqual( 1, simulationComp.Stands.Count);
+        }
+    }
+}
