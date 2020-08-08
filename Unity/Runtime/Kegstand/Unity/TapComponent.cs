637f733314c9ea7436e5e625c9dc4d63203fdfdc
Sercan Altun <altun.sercan@gmail.com>
2020-08-08 17:24:35 -0400

    Add TapComponent

---
+++ Unity/Runtime/Kegstand/Unity/TapComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Kegstand.Unity
+{
+    public class TapComponent : MonoBehaviour, Tap, IWrapperComponent<Tap>
+    {
+        private Tap wrappedTap;
+        public void SetWrappedObject(Tap wrappedObject) => wrappedTap = wrappedObject;
+
+        #region Wrapper Implementation
+        public float FlowAmount => wrappedTap.FlowAmount;
+        public void SetFlow(float f) => wrappedTap.SetFlow(f);
+        #endregion Wrapper Implementation
+
+    }
+}
